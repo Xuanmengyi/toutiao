a3aa1f338362c012e8715f776b87ebe8d087a278
xmy <xmy6423@163.com>
2022-09-06 18:12:57 +0800

    登录页完成布局

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div>
-    <span class="toutiao toutiao-yanzhengma"></span>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <div class="box"></div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 // git log 查看commit记录
-export default {}
+export default {
+  data() {
+    return {}
+  }
+}
 </script>
 
 <style scoped>
